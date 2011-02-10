7a7be65778b96dc687ef18cf5bf38f9b6f0d3cd4
kyle.braak <kyle.braak@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2011-02-10 15:06:42 +0000

    commit HttpUtilTest from IPT
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-httputils/trunk@104 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

---
+++ src/test/java/org/gbif/utils/HttpUtilTest.java
@@ -0,0 +1,34 @@
+package org.gbif.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * @author markus
+ */
+public class HttpUtilTest {
+  @Test
+  public void testConditionalGet() throws ParseException, IOException {
+    DefaultHttpClient client = new DefaultHttpClient();
+    HttpUtil util = new HttpUtil(client);
+    Date last = HttpUtil.DATE_FORMAT_RFC2616.parse("Wed, 03 Aug 2009 22:37:31 GMT");
+    Date current = HttpUtil.DATE_FORMAT_RFC2616.parse("Wed, 04 Aug 2010 8:14:57 GMT");
+
+    File tmp = File.createTempFile("vocab", ".xml");
+    URL url = new URL("http://rs.gbif.org/vocabulary/gbif/resource_type.xml");
+    boolean downloaded = util.downloadIfChanged(url, last, tmp);
+    assertTrue(downloaded);
+
+    downloaded = util.downloadIfChanged(url, current, tmp);
+    assertFalse(downloaded);
+  }
+}
