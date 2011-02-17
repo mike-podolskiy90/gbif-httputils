454099b216d7dd3245597d1ea8b5bb2433e12163
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2011-02-17 16:25:53 +0000

    remove unfunctional test
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-httputils/trunk@134 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

--- src/test/java/org/gbif/utils/HttpUtilTest.java
+++ src/test/java/org/gbif/utils/HttpUtilTest.java
@@ -21,23 +21,6 @@ public class HttpUtilTest {
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @Test
-  public void testBinaryDownload() throws ParseException, IOException {
-    HttpUtil util = new HttpUtil(HttpUtil.newMultithreadedClient());
-
-    // zip file
-    URL url = new URL("http://www.cate-araceae.org/checklist.zip");
-    File tmp = File.createTempFile("dwca", ".zip");
-    System.out.println("Downloading zip file to " + tmp.getAbsolutePath());
-    boolean downloaded = util.downloadIfChanged(url, null, tmp);
-    assertTrue(downloaded);
-
-    // html file
-    File tmp2 = File.createTempFile("resource.do", "test2");
-    System.out.println("Downloading zip file to " + tmp2.getAbsolutePath());
-    util.download("http://84.40.80.89:8080/ipt/resource.do?r=test2", tmp2);
-  }
-
-  @Test
   public void testConditionalGet() throws ParseException, IOException {
     DefaultHttpClient client = new DefaultHttpClient();
     HttpUtil util = new HttpUtil(client);
