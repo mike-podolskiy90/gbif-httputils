c5dff46fae971d2772075745652145cb6469a76a
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2011-07-19 20:28:43 +0000

    Adding static http utils convenience methods to create http entities used in POST requests
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-httputils/trunk@211 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

--- src/main/java/org/gbif/utils/HttpUtil.java
+++ src/main/java/org/gbif/utils/HttpUtil.java
@@ -9,12 +9,14 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.ProtocolVersion;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
@@ -24,9 +26,11 @@ import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
@@ -41,14 +45,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -469,5 +476,68 @@ public class HttpUtil {
       }
     }
     return false;
+  }
+
+  /**
+   * Creates a http entity suitable for POSTs that encodes a single string in utf8
+   * @param data to encode
+   * @return
+   * @throws UnsupportedEncodingException
+   */
+  public static HttpEntity stringEntity(String data) throws UnsupportedEncodingException {
+    return new StringEntity(data, "UTF-8");
+  }
+
+  /**
+   * Creates a url form encoded http entity suitable for POST requests with a single given parameter
+   * encoded in utf8
+   * @param key the parameter name
+   * @param data the value to encode
+   * @return
+   */
+  public static HttpEntity map2Entity(String key, String data) {
+    List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+    formparams.add(new BasicNameValuePair(key, data));
+    try {
+      return new UrlEncodedFormEntity(formparams, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      LOG.error("Cant encode post entity with utf8", e);
+    }
+    return null;
+  }
+
+  /**
+   * Creates a url form encoded http entity suitable for POST requests with a single given parameter
+   * encoded in utf8
+   * @param kvp the parameter map to encode
+   * @return
+   */
+  public static HttpEntity map2Entity(Map<String, String> kvp) {
+    List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+    for (String k : kvp.keySet()) {
+      formparams.add(new BasicNameValuePair(k, kvp.get(k)));
+    }
+    try {
+      return new UrlEncodedFormEntity(formparams, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      LOG.error("Cant encode post entity with utf8", e);
+    }
+    return null;
+  }
+
+  public static String responseAsString(HttpResponse response) {
+    String content = null;
+    HttpEntity entity = response.getEntity();
+    if (entity != null) {
+      try {
+        content = EntityUtils.toString(entity);
+        entity.consumeContent();
+      } catch (org.apache.http.ParseException e) {
+        LOG.error("ParseException consuming http response into string", e);
+      } catch (IOException e) {
+        LOG.error("IOException consuming http response into string", e);
+      }
+    }
+    return content;
   }
 }
